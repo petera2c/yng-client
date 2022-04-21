2b4e20baa96f4e3dcaf32325fc33f084299282d2
peter <20213436+petera2c@users.noreply.github.com>
2022-04-21 08:17:31 -0500

    tweak: adding background gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,13 +50,13 @@ const Home: NextPage = () => {
       </Head>
 
       <main
-        className="flex flex-col items-center justify-center bg-cyan-400"
+        className="flex flex-col items-center justify-center gap-6 bg-gradient-to-b from-fuchsia-700"
         style={{ minHeight: "100vh" }}
       >
         <h1 className="text-white text-6xl">
           Playing around with some stock APIs
         </h1>
-        <h4 className="text-white">
+        <h4 className="text-white text-2xl">
           Might build some SVG graphs and some sorting for a variety of tickers.
         </h4>
       </main>
