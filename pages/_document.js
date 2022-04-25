61d3adf5eda8f0d3a747b0175777a673728739a6
peter <20213436+petera2c@users.noreply.github.com>
2022-04-25 02:06:48 -0500

    tweak: moving google analytics tag to _document.tsx to stay organized

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -15,6 +16,21 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Festive&family=Nunito+Sans&display=swap"
             rel="stylesheet"
           />
+          <Script
+            id="gtag-1"
+            strategy="lazyOnload"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <Script id="gtag-2" strategy="lazyOnload">
+            {`
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+           page_path: window.location.pathname,
+           });
+       `}
+          </Script>
         </Head>
         <body>
           <Main />
