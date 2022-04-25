17bfa3e6d8dae468a3edaf00819d8270a90a0b1c
peter <20213436+petera2c@users.noreply.github.com>
2022-04-25 09:11:19 -0500

    tweak: updating readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,11 +6,12 @@ const Home = () => {
       className="flex flex-col items-center justify-center gap-6 bg-gradient-to-b from-fuchsia-700"
       title="Home"
     >
-      <h1 className="text-white text-6xl">
+      <h1 className="text-white text-6xl text-center">
         Playing around with some stock APIs
       </h1>
-      <h4 className="text-white text-2xl">
-        Might build some SVG graphs and some sorting for a variety of tickers.
+      <h4 className="text-white text-2xl text-center">
+        Might build some D3 graphs/charts for fun and some complex sorting on a
+        variety of different information for stocks
       </h4>
     </Page>
   );
