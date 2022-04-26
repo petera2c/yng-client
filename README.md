e249ce8a3c66ef801fa1ce50d1266a330506d5cb
peter <20213436+petera2c@users.noreply.github.com>
2022-04-26 00:18:54 -0500

    fix: fixing build issues - adding rel to a tag and moving scripts back to index.tsx as they are not allowed in _document.tsx:

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is a side project for fun to get familiar with Next JS, and AWS. The server and client are seperate repositories with continuous deployment connected to github.
+This is a side project for fun to get familiar with Next JS, and AWS. The server and client are seperate repositories with continuous deployment connected to github hosted on AWS.
 
 Client:
 https://www.peteryng.com/
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,46 @@
+import Script from "next/script";
+
 import Page from "../components/containers/page";
 
 const Home = () => {
   return (
-    <Page
-      className="flex flex-col items-center justify-center bg-gradient-to-b from-fuchsia-700"
-      title="Home"
-    >
-      <div className="flex flex-col gap-6 max-w-screen-lg">
-        <h1 className="text-white text-center">Side Project built on AWS</h1>
-        <h4 className="text-white text-center">
-          Front end is built with Next JS, React and Tailwind. Back-end can be
-          found here:{" "}
-          <a href="https://api.peteryng.com/" target="_blank">
-            https://api.peteryng.com/
-          </a>{" "}
-          and is built with Node, Express JS and MongoDB.
-        </h4>
-      </div>
-    </Page>
+    <>
+      <Script
+        id="gtag-1"
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id="gtag-2" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+      <Page
+        className="flex flex-col items-center justify-center bg-gradient-to-b from-fuchsia-700"
+        title="Home"
+      >
+        <div className="flex flex-col gap-6 max-w-screen-lg">
+          <h1 className="text-white text-center">Side Project built on AWS</h1>
+          <h4 className="text-white text-center">
+            Front-end is built with Next JS, React and Tailwind. Back-end can be
+            found here:{" "}
+            <a
+              href="https://api.peteryng.com/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              https://api.peteryng.com/
+            </a>{" "}
+            and is built with Node, Express JS and MongoDB.
+          </h4>
+        </div>
+      </Page>
+    </>
   );
 };
 
